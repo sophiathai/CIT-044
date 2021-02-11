dc11f3117dd86a3e468fdc2c3fd53e0fa9fb25ad
sophiathai <thai.sophia8@gmail.com>
2021-02-11 00:29:40 -0800

    Create HelloWorld.java
    Edit the comment in Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/* To compile: javac HelloWorld.java
+   To run: java HelloWorld
+*/
+// declares a class HelloWorld
+public class HelloWorld {
+    // makes this 'main' class a java app that can be executed
+    public static void main (final String[] args) {
+        // To print out " Hello World" on the output device
+        System.out.println("Hello World");
+    }
+}
