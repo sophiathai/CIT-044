0aef401e66c15b432747f9eaf17d3f50dfcab6eb
sophiathai <thai.sophia8@gmail.com>
2021-02-11 00:32:34 -0800

    Update HelloWorld.java
    Re-aligning the code for readability

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
    To run: java HelloWorld
 */
 // declares a class HelloWorld
-public class HelloWorld {
+   public class HelloWorld {
     // makes this 'main' class a java app that can be executed
     public static void main (final String[] args) {
         // To print out " Hello World" on the output device
