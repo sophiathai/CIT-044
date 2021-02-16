36e40855dcc6499c1843308018b1e9cb5017a264
sophiathai <thai.sophia8@gmail.com>
2021-02-15 22:40:45 -0800

    Finalize the code
    Edit the comment
    Rerun the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,9 @@
 */
 // declares a class HelloWorld
    public class HelloWorld {
-    // makes this 'main' class a java app that can be executed
-    public static void main (final String[] args) {
-        // To print out " Hello World" on the output device
+    // makes HelloWorld a public class that can be executed
+    public static void main (final String[] args){
+        // To print out "Hello World" on the output device
         System.out.println("Hello World");
     }
 }
